be5ef3c05de169bc3429fe1a8b9ca440834406a2
Aditya <aditya.mattoo88@gmail.com>
2020-10-19 17:37:49 +0530

    commit after test of simple raterisze needs output to image

--- Rasterizer/src/main.cpp
+++ Rasterizer/src/main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include <glm/glm.hpp>
+#include "ray.h"
 
 int main(int argc, char *argv[])
 {
+
+    ::render(1,400,10,1.5);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
