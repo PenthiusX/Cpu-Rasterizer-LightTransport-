10dde0e32d3d7fbb36d96d50917e985c49fe4213
Aditya <aditya.mattoo88@gmail.com>
2020-11-29 16:25:19 +0530

    moving onto diffuse

--- Rasterizer/src/raster2.h
+++ Rasterizer/src/raster2.h
@@ -76,14 +76,14 @@ struct hit_record
     }
 };
 //-----------------------------------------------------------------------------------------------
-class Collider{
+class Collider{//a pure virtual interface , can be extened to sphere , cube , tries(Mesh) , etc..
 
 public:
     virtual ~Collider(){};
     virtual bool hit(Ray &r,float t_min,float t_max,hit_record& rec) = 0;
 };
 
-class Sphere : public Collider{
+class Sphere : public Collider{//The sphere extension to Collider
 public:
     Sphere(){}
     Sphere(glm::vec3 c, float r , glm::vec3 co){
@@ -168,33 +168,31 @@ float clip(int n, int lower, int upper) {
     return std::max(lower, std::min(n, upper));
 }
 //-----------------------------------------------------------------------------------------------
-//-----------------------------------------------------------------------------------------------
-float hit_sphere(const glm::vec3& center, float radius, const Ray& r)
-{
-    glm::vec3 rdir = glm::normalize(r.direction());
-    glm::vec3 oc = r.origin() - center;
-    float a = glm::dot(rdir,rdir);
-    float b = 2.0 * glm::dot(oc, rdir);
-    float c = dot(oc, oc) - radius*radius;
-    float discriminant = b*b - 4*a*c;
-    //return (discriminant > 0);
+//float hit_sphere(const glm::vec3& center, float radius, const Ray& r)
+//{
+//    glm::vec3 rdir = glm::normalize(r.direction());
+//    glm::vec3 oc = r.origin() - center;
+//    float a = glm::dot(rdir,rdir);
+//    float b = 2.0 * glm::dot(oc, rdir);
+//    float c = dot(oc, oc) - radius*radius;
+//    float discriminant = b*b - 4*a*c;
+//    //return (discriminant > 0);
 
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-b - sqrt(discriminant) ) / (2.0*a);
-    }
-}
+//    if (discriminant < 0) {
+//        return -1.0;
+//    } else {
+//        return (-b - sqrt(discriminant) ) / (2.0*a);
+//    }
+//}
 
 glm::vec3 traceColor(Ray &r , Scene *s){
-
-
     hit_record rec;
     //the render order overlap is top down // first return will overlap the second
     if(s->isHit(r,0,infinity,rec)){
         return glm::vec3(0.5) * (rec.normal + rec.color);
     }
 
+    //BackGround color
     glm::vec3 unit_direction = glm::normalize(r.direction());
     float t1 = (unit_direction.y + 1) * 0.5;
     return glm::vec3(1.0-t1)*glm::vec3(1.0, 1.0, 1.0) + glm::vec3(t1)*glm::vec3(0.5, 0.7, 1.0);
@@ -207,43 +205,42 @@ void render()
     unsigned int width = 1024;
     unsigned int height = static_cast<int>(width / aspectRatio);
 
+    const int samplesPerPixel = 100;
+
     QImage image(width,height, QImage::Format_RGB32);
 
     //Camera
     Cam c;
-    c.origin = glm::vec3(0.0,0.0,3);
+    c.origin = glm::vec3(0.0,0.0,10);
     c.horizontal = glm::vec3(width,0,0);
     c.vertical = glm::vec3(0,height,0);
     c.focalLength = width/2;
 
     Scene *s = new Scene();
-    //    Collider* col = new Sphere(glm::vec3(0,0,-1),1.5,glm::vec3(0.5,0.5,1.0));
-    s->add(new Sphere(glm::vec3(0,0,-5.0),0.5,glm::vec3(0.5,0.1,0.2)));
-    s->add(new Sphere(glm::vec3(1,-2,-3.0),1.2,glm::vec3(0.5,0.5,1.0)));
-    s->add(new Sphere(glm::vec3(2,-1,-4.0),0.7,glm::vec3(0.5,0.5,0.0)));
+    s->add(new Sphere(glm::vec3(2,-2,-1.0),2.7,glm::vec3(0.5,0.5,0.0)));
+    s->add(new Sphere(glm::vec3(0,-0.5,-5.0),0.5,glm::vec3(0.5,0.1,0.2)));
+    s->add(new Sphere(glm::vec3(1,100,-3.0),100.2,glm::vec3(0.5,0.5,1.0)));
 
-    uint frames = 10;
-    for(uint t = 0 ; t < frames ; t++)
+    uint aniFrames = 20;
+    for(uint t = 0 ; t < aniFrames ; t++)
     {   //animation debug loop
 
-        c.origin.x = t*0.1;
+        //y is inverted for some reason
+        c.origin.y -= t*0.07;
+         c.origin.x -= t*0.07;
 
-
-        glm::vec3 lowerLeftCorner = c.origin - (c.horizontal*glm::vec3(0.5)) - (c.vertical*glm::vec3(0.5)) - glm::vec3(0, 0, c.focalLength);
-
-        for(/*unsigned int y = 0 ; y < height ; y++*/unsigned int y = height ; y > 0 ; y--)//starts from max heights // topLeft edge // and moves to 0 bottom left
+        for(unsigned int y = 0 ; y < height ; y++/*unsigned int y = height ; y > 0 ; y--*/)//starts from max heights // topLeft edge // and moves to 0 bottom left
         {
             for(unsigned int x = 0 ; x < width; x++)
             {
                 float u = float(x) / (width);
                 float v = float(y) / (height);
 
+
+                glm::vec3 lowerLeftCorner = c.origin - (c.horizontal*glm::vec3(0.5)) - (c.vertical*glm::vec3(0.5)) - glm::vec3(0, 0, c.focalLength);
                 glm::vec3 dir = glm::normalize(lowerLeftCorner + (glm::vec3(u)*c.horizontal) + (glm::vec3(v)*c.vertical) - c.origin);
                 Ray r(c.origin,dir);
-                glm::vec3 col = traceColor(r,s);
-
-                //              Ray r(glm::vec3(x,y,2),glm::vec3(0,0,-1));
-                //              glm::vec3 col = raycolor(r);
+                glm::vec3 col = traceColor(r,s);//check for intercetion and color that pixel
 
                 int ir = static_cast<int>(255.999 * col.x);
                 int ig = static_cast<int>(255.999 * col.y);
@@ -254,13 +251,11 @@ void render()
                 //image.save("Finalsave.jpeg", 0, -1);
             }
         }
-
-        //    saveBmp(width,height,pixels);
+      //saveBmp(width,height,pixels);
         image.save(QString::number(t) + "Ras2.jpeg", 0, -1);
-        if(t == frames-1)
+        if(t == aniFrames-1)
             qInfo() << "Debugbreak";
     }
-
     delete s;
 }
 //--- --------------------------------------------------------------------------------------------
